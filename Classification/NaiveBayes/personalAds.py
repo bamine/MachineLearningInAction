1b9edae565006ae4dc5d91a8314e833d83d654ef
bamine <amine.benhalloum@gmail.com>
2014-10-23 01:17:24 +0200

    personal ads example

---
+++ Classification/NaiveBayes/personalAds.py
@@ -0,0 +1,9 @@
+import operator
+
+
+def calcMostFreq(vocabList, fullText):
+    freqDict = {}
+    for token in vocabList:
+        freqDict[token] = fullText.count(token)
+    sortedFreq = sorted(freqDict.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedFreq[:30]
