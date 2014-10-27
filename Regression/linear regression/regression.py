754fa39a4b41ed93ed15d3678e77fc016e9bd169
bamine <amine.benhalloum@gmail.com>
2014-10-27 22:49:57 +0100

    locally weighted linear regression

--- Regression/linear regression/regression.py
+++ Regression/linear regression/regression.py
@@ -28,19 +28,41 @@ def standardRegression(x, y):
     return ws
 
 
+def locallyWeightedLinearRegression(testPoint, x, y, k=1.0):
+    xMat = np.mat(x)
+    yMat = np.mat(y).T
+    m = np.shape(xMat)[0]
+    weights = np.mat(np.eye(m))
+    for j in xrange(m):
+        diffMat = testPoint - xMat[j, :]
+        weights[j, j] = np.exp(diffMat * diffMat.T / (-2.0 * k ** 2))
+    xTx = xMat.T * (weights * xMat)
+    if np.linalg.det(xTx) == 0.0:
+        print "This matrix is singular, cannot invert"
+        return
+    ws = xTx.I * (xMat.T * (weights * yMat))
+    return testPoint * ws
+
+
+def lwlrTest(testArray, x, y, k=1.0):
+    m = np.shape(testArray)[0]
+    yHat = np.zeros(m)
+    for i in xrange(m):
+        yHat[i] = locallyWeightedLinearRegression(testArray[i], x, y, k)
+    return yHat
+
+
 if __name__ == "__main__":
     x, y = loadDataSet('ex0.txt')
-    ws = standardRegression(x, y)
+    print locallyWeightedLinearRegression(x[0], x, y, 1.0)
+    print locallyWeightedLinearRegression(x[0], x, y, 0.01)
+    yHat = lwlrTest(x, x, y, 0.01)
     xMat = np.mat(x)
-    yMat = np.mat(y)
-    yHat = xMat * ws
+    srtInd = xMat[:, 1].argsort(0)
+    xSort = xMat[srtInd][:, 0, :]
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0])
-    xCopy = xMat.copy()
-    xCopy.sort(0)
-    yHat = xCopy * ws
-    ax.plot(xCopy[:, 1].flatten().A[0], yHat[:, 0].flatten().A[0])
-    print np.corrcoef(yHat.T, yMat)
+    ax.plot(xSort[:, 1].flatten().A[0], np.mat(yHat[srtInd]).T.flatten().A[0])
+    ax.scatter(xMat[:, 1].flatten().A[0], np.mat(y).T.flatten().A[0], s=2, c='red')
     plt.show()
 
