b30591d3ccff0044e280441687bc42bbd7de459f
bamine <amine.benhalloum@gmail.com>
2014-11-02 23:20:59 +0100

    added tostring method

--- Regression/CART/tree.py
+++ Regression/CART/tree.py
@@ -1,6 +1,12 @@
 class treeNode(object):
-    def __init__(self, feat, val, right, left):
+    def __init__(self, feat=None, val=None, right=None, left=None):
         self.feature = feat
         self.value = val
         self.right = right
         self.left = left
+
+    def __str__(self):
+        return "feature : " + str(self.feature) + "\n" + \
+               "value : " + str(self.value) + "\n" + \
+               "right : " + str(self.right) + "\n" + \
+               "left : " + str(self.left) + "\n"
